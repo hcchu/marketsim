79ff06d5b51c36c34ad49e3f3ee10b1ffb66db21
hcchu <hcchu@seventreasures.net>
2014-04-30 12:26:39 -0400

    clean up, rename order books

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-    bid_book := marketsim.NewBook()
-    ask_book := marketsim.NewBook()
+    buy_book := marketsim.NewBook()
+    sell_book := marketsim.NewBook()
 
 	for {
 		line, err := reader.ReadString('\n')
@@ -22,13 +22,6 @@ func main() {
 			break
 		}
         orderline := marketsim.ParseOrder(line)
-        marketsim.DispatchOrder(orderline, &bid_book, &ask_book)
-        //order_book = append(order_book, *orderline)
-        //sort.Sort(marketsim.ByTimestamp(order_book))
-        //sort.Sort(marketsim.ByPrice(order_book))
-        //fmt.Println(order_book)
-        //fmt.Println(order_book[0])
-        fmt.Println(bid_book)
-        fmt.Println(ask_book)
+        marketsim.DispatchOrder(orderline, &buy_book, &sell_book)
 	}
 }
