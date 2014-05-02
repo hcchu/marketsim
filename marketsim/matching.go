6729ffc3743ba0ecd2c415b73de4ea43730880db
hcchu <hcchu@seventreasures.net>
2014-05-02 01:04:56 -0400

    simplify matching function

--- marketsim/matching.go
+++ marketsim/matching.go
@@ -12,3 +12,34 @@ func MatchEngine(b []Order, s []Order) {
         fmt.Println("TRADE EXECUTED")
     }
 }
+
+func removeOrder(book *[]Order) {
+    copy((*book)[0:], (*book)[1:])
+    (*book)[len(*book)-1] = Order{}
+    *book = (*book)[:len(*book)-1]
+}
+
+func MatchOrder(o *Order, sell *[]Order, buy *[]Order) bool {
+    switch {
+    case o.OrderType == "BUY" && len(*sell) == 0: 
+        return false
+    case o.OrderType == "SELL" && len(*buy) == 0: 
+        return false
+    case o.OrderType == "BUY":
+        if o.Price >= (*sell)[0].Price {
+            fmt.Println("BUY ORDER EXECUTED for",
+            o.Commodity, "at", o.Price)
+            removeOrder(sell)
+        }
+        return true
+    case o.OrderType == "SELL":
+        if o.Price <= (*buy)[0].Price {
+            fmt.Println("SELL ORDER EXECUTED for",
+            o.Commodity, "at", o.Price)
+            removeOrder(buy)
+        }
+        return true
+    }
+    return false
+}
+
