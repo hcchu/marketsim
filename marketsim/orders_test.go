a24bc651ee190073a80e98993f63a814455f4810
hcchu <hcchu@seventreasures.net>
2014-05-08 16:37:00 -0400

    add more tests

--- marketsim/orders_test.go
+++ marketsim/orders_test.go
@@ -1,6 +1,7 @@
 package marketsim
 
 import "testing"
+import "sort"
 
 func TestParseOrder(t *testing.T) {
 	in := "2014-04-01T16:19:00Z BUY 100.000 USD 100"
@@ -20,3 +21,28 @@ func TestDispatchOrder(t *testing.T) {
         t.Errorf("DispatchOrder = %v, want %v", book_in, book_out)
     }
 }
+
+func TestBuySort(t *testing.T) {
+    buy_book := []Order{{1396369141, "BUY", 100, "USD", 100}, {1396369141, "BUY", 101, "USD", 100}, {1396369140, "BUY", 101, "USD", 100}}
+    buy_book_sorted := []Order{{1396369140, "BUY", 101, "USD", 100}, {1396369141, "BUY", 101, "USD", 100}, {1396369141, "BUY", 100, "USD", 100}}
+    sort.Sort(ByTimestamp(buy_book))
+    sort.Sort(ByPrice(buy_book))
+    for index, element := range buy_book {
+        if element != buy_book_sorted[index] {
+            t.Errorf("Buy book = %v, want %v", buy_book, buy_book_sorted)
+        }
+    }
+}
+
+func TestSellSort(t *testing.T) {
+    sell_book := []Order{{1396369141, "SELL", 100, "USD", 100}, {1396369141, "SELL", 101, "USD", 100}, {1396369140, "SELL", 101, "USD", 100}}
+    sell_book_sorted := []Order{{1396369141, "SELL", 100, "USD", 100}, {1396369140, "SELL", 101, "USD", 100}, {1396369141, "SELL", 101, "USD", 100}}
+    sort.Sort(ByTimestamp(sell_book))
+    sort.Sort(ByPrice(sell_book))
+    for index, element := range sell_book {
+        if element != sell_book_sorted[index] {
+            t.Errorf("Sell book = %v, want %v", sell_book, sell_book_sorted)
+        }
+    }
+}
+
